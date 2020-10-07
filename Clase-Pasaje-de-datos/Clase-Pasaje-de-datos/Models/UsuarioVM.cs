d28ea49fe7ed57993bf3ced28a85d2f2b6941ede
psanchez <psanchez@plus54.com>
2020-10-07 19:55:27 -0300

    ejemplo View Model

---
+++ Clase-Pasaje-de-datos/Clase-Pasaje-de-datos/Models/UsuarioVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clase_Pasaje_de_datos.Models
+{
+    public class UsuarioVM
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Rol { get; set; }
+        public string FotoPerfil { get; set; }
+    }
+}
