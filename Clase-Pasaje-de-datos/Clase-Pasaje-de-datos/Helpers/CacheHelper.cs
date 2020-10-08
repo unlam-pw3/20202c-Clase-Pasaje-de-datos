8edf4286cdea7934415dbbd845538fd33ef146f0
psanchez <psanchez@plus54.com>
2020-10-07 22:40:10 -0300

    agregando lock unlock para cache para thread safe

--- Clase-Pasaje-de-datos/Clase-Pasaje-de-datos/Helpers/CacheHelper.cs
+++ Clase-Pasaje-de-datos/Clase-Pasaje-de-datos/Helpers/CacheHelper.cs
@@ -13,17 +13,27 @@ namespace Clase_Pasaje_de_datos.Helpers
         {
             get
             {
+                //https://docs.microsoft.com/es-es/dotnet/api/system.web.httpapplicationstate.lock?redirectedfrom=MSDN&view=netframework-4.8#System_Web_HttpApplicationState_Lock
+                //en caso querer que ciertas operaciones sobre objetos compartidos en application se debe usar lock y unlock para mejorar la eficiencia
+                HttpContext.Current.Application.Lock();
+
                 if (HttpContext.Current.Cache["MisSeriesFavoritasCache"] == null)
                 {
                     HttpContext.Current.Cache.Insert("MisSeriesFavoritasCache", new List<Serie>(), null, DateTime.Now.AddSeconds(20), new TimeSpan()) ;
                 }
+                List<Serie> series = (List<Serie>)HttpContext.Current.Cache["MisSeriesFavoritasCache"];
 
-                return (List<Serie>)HttpContext.Current.Cache["MisSeriesFavoritasCache"];
+                HttpContext.Current.Application.UnLock();
+
+                return series;
+
             }
 
             set
             {
+                HttpContext.Current.Application.Lock();
                 HttpContext.Current.Cache.Insert("MisSeriesFavoritasCache", value, null, DateTime.Now.AddSeconds(20), new TimeSpan()) ;
+                HttpContext.Current.Application.UnLock();
             }
         }
     }
